53c0d39c4a3db5e5486b9ce5a812ac6bdaef5178
Ramune Juchnaite <ramune.juchnaite@stud.techin.lt>
2024-01-05 18:06:22 +0200

    PD3

---
+++ PD3/PD3_1.java
@@ -0,0 +1,24 @@
+
+class PD3_1 {
+    public static void main(String[] args) {
+        byte r = 50;
+        short temperature;
+        temperature = -500;
+                int b=20;
+                        long c=-42332200000L;
+                                float e=42.3f;
+                                        double d= 3.5;
+                                                boolean flag = true;
+                                                        char f='\uffff';
+
+        System.out.println(r);
+        System.out.println(temperature);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(flag);
+        System.out.println(f);
+
+    }
+}
