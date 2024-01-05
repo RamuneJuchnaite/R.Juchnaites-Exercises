d930285dbd7793bd50f50dc1fbe7d0b251604e79
Ramune Juchnaite <ramune.juchnaite@stud.techin.lt>
2024-01-05 18:30:54 +0200

    PD4

---
+++ PD2/PD2_5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PD2_5 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in); // apsirasome ivedima
+        System.out.println("Iveskite savo varda:"); // ispaudinam ko norime
+        String Firtsname = input .next();  //ivedam varda
+        System.out.println("Sveika \n " +Firtsname);
+
+//        System.out.println("Iveskite savo varda:");
+//        System.out.println("Ramune");
+
+    }
+}
