b0fac7c7e8c2cc1d7add9641ad39e472d4e80a66
Ramune Juchnaite <ramune.juchnaite@stud.techin.lt>
2024-01-13 18:28:32 +0200

    PD7

---
+++ PD7/PD7_11.java
@@ -0,0 +1,32 @@
+/**
+ * Parašykite Java metodą, kuris suranda ilgiausią žodį pateiktoje eilutėje.
+ */
+
+package lt.techin.pd7;
+
+public class PD7_11 {
+    public static void main(String[] args) {
+        String text = ("Java is scariest today");
+        System.out.println(sentence(text));
+            }
+            public static String sentence (String text){
+            String word = " ", longWord=" " ;// nusimatom jog tusti zodziai
+
+            for (int i = 0; i < text.length(); i++) {
+                if (text.charAt(i) != ' ') { // tarpus randa
+                    word += text.charAt(i);
+                    //System.out.println(word);
+                } else {
+                    if (word.length() > longWord.length()){
+                        longWord = word;
+                       // System.out.println(longWord);
+                    }
+                    word = " ";
+                }
+            }
+                return longWord;
+
+            }
+        }
+
+
