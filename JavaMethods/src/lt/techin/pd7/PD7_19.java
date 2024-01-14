dbc3168c2a911e16a34aafc3bd5b91a6a4e78f82
Ramune Juchnaite <ramune.juchnaite@stud.techin.lt>
2024-01-14 13:31:23 +0200

    PD7

--- JavaMethods/src/lt/techin/pd7/PD7_19.java
+++ JavaMethods/src/lt/techin/pd7/PD7_19.java
@@ -9,19 +9,19 @@ import java.util.logging.Handler;
 
 public class PD7_19 {
     public static void main(String[] args) {
-        //double printKmi = kMi(height, kg);
-       // System.out.println(printKmi);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter you Height : ");
+        double heighTInput = scanner.nextInt();
+        System.out.println("Please enter your weight: ");
+       double kgInput = scanner.nextInt();
+
+        System.out.println(kMi(heighTInput,kgInput));
     }
     private static double kMi(double height, double kg) {
         double KMI;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter you Height : ");
-        height = scanner.nextInt();
-        System.out.println("Please enter your weight: ");
-        kg = scanner.nextInt();
         KMI = (kg * kg) / (height);
 
-        if (18 < KMI || KMI < 24) {
+        if (18 < KMI && KMI < 24) {
             System.out.println("You weight is perfect ");
         } else if (KMI < 18) {
             System.out.println("Please eat more");
