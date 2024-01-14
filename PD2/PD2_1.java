b13e4b97c4574a29a17875027786429e16c64dce
Ramune Juchnaite <ramune.juchnaite@stud.techin.lt>
2024-01-14 19:43:02 +0200

    PD7

--- PD2/PD2_1.java
+++
@@ -1,8 +0,0 @@
-package lt.techin.exercises;
-
-public class PD2_1 {
-    public static void main(String[] args) {
-        System.out.println("Wellcome to Java");
-        System.out.println("Learning Java Now");
-        System.out.println("Programming is fun");
-}
