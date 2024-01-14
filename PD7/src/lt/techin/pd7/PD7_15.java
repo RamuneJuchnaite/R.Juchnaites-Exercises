be877e184cd9cb7e9a0982e80d441072bdd39c1d
Ramune Juchnaite <ramune.juchnaite@stud.techin.lt>
2024-01-14 19:38:23 +0200

    PD7

---
+++ PD7/src/lt/techin/pd7/PD7_15.java
@@ -0,0 +1,11 @@
+/**
+ * Parašykite Java metodą, kuris skaičių masyve suranda antrąjį mažiausią ir antrąjį didžiausius skaičius.
+ */
+
+package lt.techin.pd7;
+
+public class PD7_15 {
+    public static void main(String[] args) {
+        // TODO
+    }
+}
